8ae8175fe1ba59467716ffe304741ceb1f04d979
cedret <cedret3@mail.fr>
2025-06-04 20:47:56 +0200

    Update sc02savedata.sh

--- sc02savedata.sh
+++ sc02savedata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # === Configuration ===
-SOURCE_BASE="/Users/access/Documents/_MNI05_Dox_Per"                  # Dossier local contenant les tosave*
+SOURCE_BASE="/Users/access/Documents/_MNI06_Dox_Fam"                  # Dossier local contenant les tosave*
 DEST_USER="access"                       # Nom d'utilisateur distant
 DEST_HOST="192.168.1.207"                       # IP ou hostname de la machine distante
 DEST_PATH="/Volumes/vsy21tri2int/"           # Chemin sur la machine distante
@@ -168,19 +168,13 @@ do
   		;;
 	23)
 	 	echo "23 scp depuis mni(macos) =$SOURCE_BASE= vers =$DEST_SCP="
-   		du -sh $SOURCE_BASE >> savedata.log
-#		ls /Volumes/
-#		sudo ls $DEST_SCP
   		echo ">>>>> ATTENTION AU MODE DE MONTAGE DE(S) DOSSIER(S) DISTANT(S) !!!!!"
 		echo "===== Pause : appuyez sur une touche pour scp ....."
 		read -n 1 -s -r  # -n 1 : lit un caractère, -s : silencieux, -r : brut
 		scp -v -r $SOURCE_BASE $DEST_HOST:$DEST_SCP
-#		sudo rsync -avh --progress /Users/access/Documents/_MNI0* /Volumes/vsy21tri2int/ccc2506mni/ > rsync0output.log 2>&1
   		echo "===== scp -v -r =$SOURCE_BASE =$DEST_HOST:$DEST_SCP" >> savedata.log
-#		tail -n 5 rsync0output.log >> rsync.log
-#  		echo "--- tail5rsync0.log"
-		echo "===== Fin à:" >> savedata.log
-		date >> savedata.log
+   		du -sh $SOURCE_BASE >> savedata.log
+		echo "===== Fin à:" >> savedata.log && date >> savedata.log
   		;;
     	31)
 		echo "----- SUPPRESSION depuis MACOS/Debian"
