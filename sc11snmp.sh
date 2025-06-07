565c6f7e80f110789c447db0a524522917cdd089
cedret <cedret3@mail.fr>
2025-06-07 10:48:49 +0200

    Update sc11snmp.sh

--- sc11snmp.sh
+++ sc11snmp.sh
@@ -19,8 +19,9 @@ do
         HOSTNAME=$(snmpget -v$SNMP_VERSION -c $COMMUNITY $NAS_IP 1.3.6.1.2.1.1.5.0 -Ovq)
         display "Nom de l'hôte" "$HOSTNAME"
 
-	NET_SPEED=$(snmpwalk -v$SNMP_VERSION -c $COMMUNITY $NAS_IP 1.3.6.1.2.1.2.2.1.2)
-
+	NET_FACE=$(snmpwalk -v$SNMP_VERSION -c $COMMUNITY $NAS_IP 1.3.6.1.2.1.2.2.1.2 -Ovq)
+        display "Interface" "$NET_FACE"
+	
         # Uptime
         UPTIME=$(snmpget -v$SNMP_VERSION -c $COMMUNITY $NAS_IP 1.3.6.1.2.1.1.3.0 -Ovq)
         display "Uptime" "$UPTIME"
