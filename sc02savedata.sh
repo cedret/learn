e04b1e98472ade11384d0ef6e3f714c3c94c1b7d
cedret <cedret3@mail.fr>
2025-06-08 18:29:49 +0200

    Update sc02savedata.sh

--- sc02savedata.sh
+++ sc02savedata.sh
@@ -171,10 +171,12 @@ do
 		echo "Démonter/remonter réseau entre chaque itération?"
 		for index in "${indices[@]}"; do
 		  if [[ "$index" =~ ^[0-9]+$ ]] && [ "$index" -lt "${#DIRS[@]}" ]; then
-			SECONDS=0
 			src="${DIRS[$index]}"
 			dest="$DESTINATION/rsy$(basename "$src")"
-      		    du -sh $src | tee -a "$LOGFIX"
+			TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+			TAILLE=$(du -sh "$src")
+			echo "[$TIMESTAMP] $TAILLE" >> "$LOGFIX"
+			SECONDS=0
 #		    rsync -az --inplace --no-owner --no-group --progress "$src/" "$dest/"
 #		    rsync -a --inplace --no-owner --no-group --progress "$src/" "$dest/" >> /path/to/LOGFIX.log 2>&1 && echo "[$(date '+%Y-%m-%d %H:%M:%S')] Copie terminée avec succès" >> /path/to/LOGFIX.log || echo "[$(date '+%Y-%m-%d %H:%M:%S')] Erreur lors de la copie" >> "$LOGFIX"
 #		    rsync -a --inplace --no-owner --no-group --progress "$src/" "$dest/" >> /path/to/LOGFIX.log 2>&1 && echo "[$(date '+%Y-%m-%d %H:%M:%S')] Copie terminée avec succès. Total des fichiers transférés : $(find "$dest" -type f | wc -l)" >> /path/to/LOGFIX.log || echo "[$(date '+%Y-%m-%d %H:%M:%S')] Erreur lors de la copie" >> "$LOGFIX"
@@ -187,7 +189,6 @@ do
 #  			echo "[$(date '+%Y-%m-%d %H:%M:%S')] --- ℹ ℹ Rsync sans erreur ℹ ℹ --- Total des fichiers transférés : $(find "$dest" -type f | wc -l)" | tee -a "$LOGFIX" || \
 #     			echo "[$(date '+%Y-%m-%d %H:%M:%S')] --- ⚠️ ⚠️ Rsync avec erreur(s): (code $status) ⚠️ ⚠️ -----" | tee -a "$LOGFIX"
 			rsync -a --inplace --no-owner --no-group --progress --timeout=60 --stats "$src/" "$dest/" >> "$LOGFMR" 2>&1
-
    			status=$?
    			minutes=$((SECONDS / 60))
 			# Vérification du code de sortie de rsync
