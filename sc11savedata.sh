729617bc585bac17a7c450229587e8739779aea8
cedret <cedret3@mail.fr>
2025-06-09 17:52:02 +0200

    Update sc11savedata.sh

--- sc11savedata.sh
+++ sc11savedata.sh
@@ -254,7 +254,7 @@ do
 # Format and print the output
 #			echo "[$TIMESTAMP2] $TAILLE transferred in $MINUTES minutes at a speed of $SPEED_MBPS MB/s" | tee -a "$LOGFIX"
 # Vérification du code de sortie de rsync
-			echo "[$TIMESTAMP1] - [$TIMESTAMP2]"
+			echo "[$TIMESTAMP1] - [$TIMESTAMP2]" | tee -a "$LOGFIX"
 			echo "De T1: $TAILLE1" | tee -a "$LOGFIX"
    			echo "A T2: $TAILLE2" | tee -a "$LOGFIX"
       			echo "En $MINUTES min. à $SPEED_BPS O/s" | tee -a "$LOGFIX"
