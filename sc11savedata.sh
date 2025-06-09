e1c6ec997df6ad179e9045327519971415389d4b
cedret <cedret3@mail.fr>
2025-06-09 18:53:44 +0200

    Update sc11savedata.sh

--- sc11savedata.sh
+++ sc11savedata.sh
@@ -207,7 +207,7 @@ do
 			RSYNC1CMD="rsync -a --inplace --no-owner --no-group --progress --timeout=60 --stats "$src/" "$dst/" >> "$LOGFMR" 2>&1"
 			TIMESTAMP1=$(date '+%Y-%m-%d %H:%M:%S')
 			SECONDS=0
-			echo "--- RSYNC de: $src. Démonter/remonter réseau entre chaque itération?"
+			echo "--- RSYNC de: $TAILLE1. Démonter/remonter réseau entre chaque itération?"
 #		    rsync -az --inplace --no-owner --no-group --progress "$src/" "$dst/"
 #		    rsync -a --inplace --no-owner --no-group --progress "$src/" "$dst/" >> /path/to/LOGFIX.log 2>&1 && echo "[$(date '+%Y-%m-%d %H:%M:%S')] Copie terminée avec succès" >> /path/to/LOGFIX.log || echo "[$(date '+%Y-%m-%d %H:%M:%S')] Erreur lors de la copie" >> "$LOGFIX"
 #		    rsync -a --inplace --no-owner --no-group --progress "$src/" "$dst/" >> /path/to/LOGFIX.log 2>&1 && echo "[$(date '+%Y-%m-%d %H:%M:%S')] Copie terminée avec succès. Total des fichiers transférés : $(find "$dst" -type f | wc -l)" >> /path/to/LOGFIX.log || echo "[$(date '+%Y-%m-%d %H:%M:%S')] Erreur lors de la copie" >> "$LOGFIX"
@@ -227,7 +227,7 @@ do
 			cycle=0
 			# Affichage toutes les 60s pendant l'exécution
 			while kill -0 "$RSYNC_PID" 2>/dev/null; do
-			    echo "-$cycle-"
+			    echo "--- $cycle minutes --- extrait de logfmr ---"
 			    tail -n 2 "$LOGFMR"
 			    sleep 60
 			    ((cycle++))
