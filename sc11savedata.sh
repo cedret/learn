19c94da1380ca54e6b562272b69f49a7ce2d997c
cedret <cedret3@mail.fr>
2025-06-10 07:52:35 +0200

    Update sc11savedata.sh

--- sc11savedata.sh
+++ sc11savedata.sh
@@ -228,7 +228,7 @@ do
 			cycle=0
 			# Affichage toutes les 60s pendant l'exécution
 			while kill -0 "$RSYNC_PID" 2>/dev/null; do
-			    echo "--- $cycle minutes --- extrait de logfmr ---"
+			    echo "--- $cycle minutes --- extrait de logfmr --- $(date '+%Y-%m-%d %H:%M:%S')"
 			    tail -n 1 "$LOGFMR"
 			    sleep 60
 			    ((cycle++))
